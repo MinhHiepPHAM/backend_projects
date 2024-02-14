7eeeabd7c543ec8a8c984295aff9b4921ee190b0
minh-hiep <minhhiepfiev57@gmail.com>
2024-02-14 23:41:42 +0100

    update sign_in function for post request

--- myproject/authentication/views.py
+++ myproject/authentication/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 from authentication.forms import LoginForm
 
@@ -7,5 +9,20 @@ def sign_in(request):
     if request.method == 'GET':
         form = LoginForm()
         return render(request, 'users/login.html', {'form': form})
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            user = authenticate(request,password=password,email=email)
+            if user:
+                login(request, user)
+                messages.success(request, f'Hi {user.first_name} {user.last_name}')
+                return redirect('home')
+
+        messages.error(request,'Invalid username or password')
+        return render(request, "users/login.html", {'form':form})
+
+
 
 
