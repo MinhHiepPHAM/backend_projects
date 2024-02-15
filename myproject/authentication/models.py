35139e8e46be38f8b323255dbb099b21bcf3f87e
PHAM Minh Hiep <53022146+MinhHiepPHAM@users.noreply.github.com>
2024-02-15 17:53:53 +0100

    use UserManager for object of customUser

--- myproject/authentication/models.py
+++ myproject/authentication/models.py
@@ -1,29 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email,password,**extra_fields)
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField("email address", unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-    objects = CustomUserManager
-
+    REQUIRED_FIELDS = ['first_name', 'last_name','username']
+    objects = UserManager
