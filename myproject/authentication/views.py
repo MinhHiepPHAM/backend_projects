dee010a6aafe9bf19dcc21c7f6e87161c4c99b79
minh-hiep <minhhiepfiev57@gmail.com>
2024-02-20 23:32:08 +0100

    clean views.py

--- myproject/authentication/views.py
+++ myproject/authentication/views.py
@@ -1,16 +1,44 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.http import HttpResponse
+#from django.contrib import messages
+#from django.contrib.auth import authenticate, login
+#from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from authentication.forms import LoginForm, RegisterForm
-from authentication.models import CustomUser
-from django.contrib.auth.views import LoginView
-from django.views.generic.edit import *
-from django.contrib.auth.decorators import login_required
+#from authentication.models import CustomUser
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import FormView
+#from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import View
+    
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+    template_name = 'users/login.html'
+    authentication_form = LoginForm
+
+    def form_valid(self, form):
+        # Custom logic when the form is valid (e.g., log the user activity)
+        username = form.cleaned_data['username']
+        self.request.session['username'] = username
+        return redirect('dashboard')
+    
+   # def form_invalid(self, form):
+    #    return HttpResponse(f'Unsuccess {form.cleaned_data}')
+
+class DashboardView(LoginRequiredMixin, View):
+    login_url = "/dashboard"
+    def get(self, request, *args, **kwargs):
+        username = self.request.session['username']
+        return render(request,"users/dashboard.html",{'username':username})
+
+
+class RegisterView(FormView):
+    template_name = "users/register.html"
+    form_class = RegisterForm
+
+    def form_valid(self, form):
+        form.save()
+        return redirect('login')
 
 
 # def sign_in(request):
@@ -45,35 +73,7 @@ from django.views.generic.base import View
 #     else:
 #         form = RegisterForm()
 #         return render(request, "users/register.html", {'form': form})
-    
-class CustomLoginView(LoginView):
-    form_class = LoginForm
-    template_name = 'users/login.html'
-    authentication_form = LoginForm
 
-    def form_valid(self, form):
-        # Custom logic when the form is valid (e.g., log the user activity)
-        username = form.cleaned_data['username']
-        self.request.session['username'] = username
-        return redirect('dashboard')
-    
-   # def form_invalid(self, form):
-    #    return HttpResponse(f'Unsuccess {form.cleaned_data}')
-
-class DashboardView(LoginRequiredMixin, View):
-    login_url = "/dashboard"
-    def get(self, request, *args, **kwargs):
-        username = self.request.session['username']
-        return render(request,"users/dashboard.html",{'username':username})
-
-
-class RegisterView(FormView):
-    template_name = "users/register.html"
-    form_class = RegisterForm
-
-    def form_valid(self, form):
-        form.save()
-        return redirect('login')
 
 
 
