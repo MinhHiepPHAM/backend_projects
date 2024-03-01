9e154b5fb91a619c725f8e1016253e46827bf7fe
minh-hiep <minhhiepfiev57@gmail.com>
2024-03-01 18:25:27 +0100

    update reset password: done

--- myproject/authentication/urls.py
+++ myproject/authentication/urls.py
@@ -15,13 +15,13 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
+    path(r'reset/<str:uidb64>/<str:token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     re_path(r'login/$', views.CustomLoginView.as_view(), name='login'),
     re_path(r'signup/$', views.RegisterView.as_view(), name='register'),
     re_path(r'dashboard/$', views.DashboardView.as_view(), name='dashboard'),
     re_path(r'logout/$', views.CustomLogoutView.as_view(), name='logout'),
     path('home/', views.HomePageView.as_view(), name='home'),
     path(r'password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
-    path('reset/<str:uidb64>/<str:token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password_reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/done/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
