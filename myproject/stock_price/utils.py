91668b13edbb238d8729d7f48890dc431d460965
minh-hiep <minhhiepfiev57@gmail.com>
2024-03-08 22:05:50 +0100

    display many stock prices

--- myproject/stock_price/utils.py
+++ myproject/stock_price/utils.py
@@ -2,19 +2,23 @@ import requests
 from django.conf import settings
 
 
-def get_stock_price(symbol):
+def get_stock_price(symbols):
     base_url = 'https://www.alphavantage.co/query'
     function = 'GLOBAL_QUOTE'
-    params = {
-        'symbol': symbol,
-        'apikey': settings.ALPHA_VANTAGE_API_KEY,
-        'function': function,
-    }
 
-    try:
-        response = requests.get(base_url, params=params)
-        data = response.json()
-        return data['Global Quote']['05. price']  # Adjust this based on the actual API response
-    except requests.RequestException as e:
-        print(f"Error fetching stock data: {e}")
-        return None
+    stock_prices = dict()
+    for symbol in symbols:
+        params = {
+            'symbol': symbol,
+            'apikey': settings.ALPHA_VANTAGE_API_KEY,
+            'function': function,
+        }
+
+        try:
+            response = requests.get(base_url, params=params)
+            data = response.json()
+            stock_prices[symbol] =  data['Global Quote']['05. price']  # Adjust this based on the actual API response
+        except requests.RequestException as e:
+            print(f"Error fetching stock data: {e}")
+            
+    return stock_prices
--- myproject/stock_price/views.py
+++ myproject/stock_price/views.py
@@ -6,9 +6,9 @@ from django.shortcuts import render
 from .utils import get_stock_price
 
 def stock_price(request):
-    symbol = 'QCOM'  # Replace with the desired stock symbol
+    symbols = ['QCOM', 'AAPL', 'GOOGL']  # Replace with the desired stock symbol
 
-    stock_price = get_stock_price(symbol)
+    stock_prices = get_stock_price(symbols)
 
-    return render(request, 'stock/stock_price.html', {'stock_price': stock_price})
+    return render(request, 'stock/stock_price.html', {'stock_prices': stock_prices})
 
