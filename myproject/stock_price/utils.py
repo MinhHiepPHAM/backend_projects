f0e1c61948f9ee9a883e5ba70fd30022ceefefeb
minh-hiep <minhhiepfiev57@gmail.com>
2024-03-10 00:17:55 +0100

    using yahoo finance api instead of alpha advantage api to avoid limit request perday and update stock page

--- myproject/stock_price/utils.py
+++ myproject/stock_price/utils.py
@@ -1,26 +1,32 @@
 import requests
 from django.conf import settings
+import yfinance as yf
 
 
 def get_stock_price(symbols):
-    base_url = 'https://www.alphavantage.co/query'
-    function = 'GLOBAL_QUOTE'
+    # base_url = 'https://www.alphavantage.co/query'
+    # function = 'GLOBAL_QUOTE'
 
     stock_prices = dict()
+    stock_volumes = dict()
+    stock_changes = dict()
     for symbol in symbols:
-        params = {
-            'symbol': symbol,
-            'apikey': settings.ALPHA_VANTAGE_API_KEY,
-            'function': function,
-        }
+        # params = {
+        #     'symbol': symbol,
+        #     'apikey': settings.ALPHA_VANTAGE_API_KEY,
+        #     'function': function,
+        # }
 
         try:
-            response = requests.get(base_url, params=params)
-            data = response.json()
-            # print(f"API Response for {symbol}: {data}")
-            stock_prices[symbol] =  data['Global Quote']['05. price']  # Adjust this based on the actual API response
+            data = yf.download(symbol, period="1d")
+            #print(data)
+            stock_prices[symbol] =  float("{:.2f}".format(data['Close'][0]))  # Adjust this based on the actual API response
+            stock_volumes[symbol] = data['Volume'][0]
+            stock_changes[symbol] = float("{:.2f}".format(data['High'][0]-data['Low'][0]))
         except requests.RequestException as e:
             print(f"Error fetching stock data: {e}, {symbol}")
-            stock_prices[symbol] = None
+            stock_prices[symbol] = 'N/A'
+            stock_volumes[symbol] = 'N/A'
+            stock_changes[symbol] = 'N/A'
             
-    return stock_prices
+    return stock_prices, stock_volumes, stock_changes
--- myproject/stock_price/views.py
+++ myproject/stock_price/views.py
@@ -6,9 +6,9 @@ from django.shortcuts import render
 from .utils import get_stock_price
 
 def stock_price(request):
-    symbols = ['QCOM', 'AAPL', 'GOOGL']  # Replace with the desired stock symbol
+    symbols = ['QCOM', 'AAPL', 'GOOGL'] 
 
-    stock_prices = get_stock_price(symbols)
+    stock_prices, stock_volumes, stock_changes = get_stock_price(symbols)
 
-    return render(request, 'stock/stock_price.html', {'stock_prices': stock_prices, 'user':request.user})
+    return render(request, 'stock/stock_price.html', {'stock_prices': stock_prices,'stock_volumes': stock_volumes, 'stock_changes': stock_changes, 'user':request.user})
 
