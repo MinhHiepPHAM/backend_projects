828613f3851d198d4ccf92a86ea40f7f283f9316
minh-hiep <minhhiepfiev57@gmail.com>
2024-03-20 21:57:26 +0100

    migrate sqlite to postgresql

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_database',
+        'USER': 'minh_hiep',
+        'HOST': 'localhost',
+        'PASSWORD': 'minh-hiep123',
+        'port': 5432
     }
 }
 
